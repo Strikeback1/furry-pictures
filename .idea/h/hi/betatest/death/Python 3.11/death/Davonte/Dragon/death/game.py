16971e6b558ba3b1ebe43f213923cf832f9cfdab
Strikeback1 <124282998+Strikeback1@users.noreply.github.com>
2023-02-03 10:07:38 -0500

    Create game.py

---
+++ .idea/h/hi/betatest/death/Python 3.11/death/Davonte/Dragon/death/game.py
@@ -0,0 +1,8 @@
+name = input("What is your name? ")
+print("Hello " + name)
+input("How was your day? ")
+input("That is good. Are you going you ask how is my day ")
+emotion = good
+emotion = fine
+emotion = bad
+print("I am doing " + emotion)
