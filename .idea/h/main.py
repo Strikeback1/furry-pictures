fd0cff070f0db852297ef6ed644e1ff74562b013
Strikeback1 <124282998+Strikeback1@users.noreply.github.com>
2023-02-03 10:16:29 -0500

    Create main.py

---
+++ .idea/h/main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    input("Hello furries cna you give me any information. ")
+    name = input("What is your fursonas name? ")
+    print("Hello " + name )
+    input("What is your fursonas age? ")
+    input("What can your fursona do? ")
+    input("What is the colors of your fursona? ")
+    input("What animal is your fursona? ")
+    # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
