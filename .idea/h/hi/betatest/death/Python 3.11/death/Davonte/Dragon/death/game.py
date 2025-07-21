f702e8822650e5a0535f5cce2bc6228f5ede0374
furry butler <124282998+Strikeback1@users.noreply.github.com>
2025-07-21 12:19:59 -0400

    Delete .idea/h directory

--- .idea/h/hi/betatest/death/Python 3.11/death/Davonte/Dragon/death/game.py
+++
@@ -1,8 +0,0 @@
-name = input("What is your name? ")
-print("Hello " + name)
-input("How was your day? ")
-input("That is good. Are you going you ask how is my day ")
-emotion = good
-emotion = fine
-emotion = bad
-print("I am doing " + emotion)
